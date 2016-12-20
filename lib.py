dbffb7358f6c55809dcd1dbe14db881a37cc7d59
Roy Evangelista <r.evangelista@arcanys.com>
2016-12-20 10:41:59 +0800

    Add my solution and unit tests.

---
+++ lib.py
@@ -0,0 +1,20 @@
+def four_digits(A, B, C, D):
+
+    l = [A, B, C, D]
+    s = [(2,), (3, 9), (6,), (9,)]
+    h = []
+
+    try:
+        for i, v in enumerate(s):
+            if len(h) == 1:
+                m = max(list(filter(lambda x: x <= v[1] if h[0] <= 1 else x <= v[0], l)))
+            else:
+                m = max(list(filter(lambda x: x <= v[0], l)))
+            h.append(m)
+            l.remove(m)
+    except ValueError:
+        return "NOT POSSIBLE"
+
+    return "%s%s:%s%s" % tuple(h)
+
+    return "NOT POSSIBLE"
