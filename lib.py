8b5919821e7b3ee581a223fc1f8353ca38d23780
Roy Evangelista <royevangelista@gmail.com>
2019-01-18 14:43:01 +0800

    Use permutations instead
    Fixes #1

--- lib.py
+++ lib.py
@@ -1,18 +1,28 @@
+import datetime
+from itertools import permutations
+
 def max_valid_time(A, B, C, D):
+    def valid_time(args, last_time):
+        time_text = "{0}{1}:{2}{3}".format(*args)
+        try:
+            time = datetime.datetime.strptime(time_text, "%H:%M")
+            if last_time is None:
+                return time
 
-    l = [A, B, C, D]
-    s = [(2,), (3, 9), (6,), (9,)]
-    h = []
+            if time > last_time:
+                return time
 
-    try:
-        for i, v in enumerate(s):
-            if len(h) == 1:
-                m = max(list(filter(lambda x: x <= v[1] if h[0] <= 1 else x <= v[0], l)))
-            else:
-                m = max(list(filter(lambda x: x <= v[0], l)))
-            h.append(m)
-            l.remove(m)
-    except ValueError:
+            return last_time
+        except ValueError, ex:
+            return last_time
+
+    biggest_time = None
+    combinations = list(permutations([A, B, C, D]))
+    for x in combinations:
+        biggest_time = valid_time(x, biggest_time)
+
+    if biggest_time is None:
         return "NOT POSSIBLE"
 
-    return "%s%s:%s%s" % tuple(h)
+    return biggest_time.strftime("%H:%M")
+
