e7dc5838d23c4c721239dc0b7a3d63ee98b49099
Roy Evangelista <royevangelista@gmail.com>
2019-01-22 08:40:20 +0800

    Update lib.py

--- lib.py
+++ lib.py
@@ -6,13 +6,14 @@ def max_valid_time(A, B, C, D):
         time_text = "{0}{1}:{2}{3}".format(*args)
         try:
             time = datetime.datetime.strptime(time_text, "%H:%M")
-            if last_time is None:
-                return time
-
-            if time > last_time:
-                return time
         except ValueError:
-            pass
+            return last_time
+
+        if last_time is None:
+            return time
+
+        if time > last_time:
+            return time
 
         return last_time
 
